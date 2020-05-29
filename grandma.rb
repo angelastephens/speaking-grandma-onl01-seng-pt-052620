4ef703a9f858e45e4cc2a4d710dc1d19eb7871a1
Angela Stephens <angelastephens317@gmail.com>
2020-05-29 03:26:49 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma (phrase)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase.downcase
    return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase.upcase
+  elsif phrase.upcase==phrase
    return "NO, NOT SINCE 1938!"
   else 
     return "I LOVE YOU TOO PUMPKIN"
