89fb28b723f858f6cef08127d6fa78cf8a29858b
Angela Stephens <angelastephens317@gmail.com>
2020-05-29 03:41:21 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma (phrase)
   if phrase==phrase.upcase
     return "NO, NOT SINCE 1938!"
-  elsif phras.downcase==phrase.downcase
+  elsif phrase.downcase==phrase.downcase
    return "HUH?! SPEAK UP, SONNY!"
   else 
     return "I LOVE YOU TOO PUMPKIN!"
