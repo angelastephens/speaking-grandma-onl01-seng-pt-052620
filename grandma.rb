5227d0448039044390dc80efd48520548f32a91c
Angela Stephens <angelastephens317@gmail.com>
2020-05-29 01:55:08 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,5 @@
 # Write a speak_to_grandma method.
-def speak_to_grandma ["I LOVE YOU GRANDMA"]
+def speak_to_grandma ["phrase"]
   if "i love you grandma.uppercase"
      return "HUH?! SPEAK UP, SONNY!"
 end
